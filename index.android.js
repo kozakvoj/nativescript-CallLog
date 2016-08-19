733232fc2803bcb7a70314dc68f2323237148d79
Vojtěch Kozák <kozak.v@gmail.com>
2016-08-19 20:33:18 +0200

    #5 Add option to return known/unknown numbers only

--- index.android.js
+++ index.android.js
@@ -1,20 +1,28 @@
 var appModule = require("application");
 var CallLog = require("./calllog-model");
 var Type = require("./call-types");
+var ContactType = require("./contact-types");
 
 exports.getCallLog = function (args) {
     return new Promise(function (resolve, reject) {
-        var projection = null;
+        var whereClause = "";
 
-        if (args && args.filter && args.filter.length > 0) projection = "type = " + args.filter[0];
+        if (args && args.filter && args.filter.length > 0) whereClause = "type = " + args.filter[0];
         if (args && args.filter && args.filter.length > 1) {
             for (var x = 1; x < args.filter.length; x++) {
-                projection += " OR type = " + args.filter[x];
+                whereClause += " OR type = " + args.filter[x];
             }
         }
 
+        if (args && args.contactType && whereClause) whereClause += " AND ";
+        if (args && args.contactType && args.contactType === ContactType.KNOWN) {
+            whereClause += "contactid > 0";
+        } else if (args && args.contactType && args.contactType === ContactType.UNKNOWN) {
+            whereClause += "contactid = 0";
+        }
+
         var CallLogs = android.provider.CallLog.Calls;
-        var c = appModule.android.context.getContentResolver().query(CallLogs.CONTENT_URI, null, projection, null, null);
+        var c = appModule.android.context.getContentResolver().query(CallLogs.CONTENT_URI, null, whereClause, null, null);
 
         if (c.getCount() > 0) {
             var cl = [];
@@ -40,4 +48,4 @@ exports.getCallLog = function (args) {
 };
 
 exports.Type = Type;
-
+exports.ContactType = ContactType;
