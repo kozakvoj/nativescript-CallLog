0aa02fa4c552ff0888dddf21888f5ecce2f10762
Vojtěch Kozák <kozak.v@gmail.com>
2016-08-19 21:39:40 +0200

    trailing comma

--- call-types.js
+++ call-types.js
@@ -1,5 +1,5 @@
 module.exports = {
     OUTCOMING: android.provider.CallLog.Calls.OUTGOING_TYPE,
     INCOMING : android.provider.CallLog.Calls.INCOMING_TYPE,
-    MISSED   : android.provider.CallLog.Calls.MISSED_TYPE,
+    MISSED   : android.provider.CallLog.Calls.MISSED_TYPE
 };
