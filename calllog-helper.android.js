2217c9b626c37981ffa4e00de665d46e3e86fb9d
Vojtěch Kozák <kozak.v@gmail.com>
2016-08-19 19:02:22 +0200

    #4 Wrong date field - overflown int

--- calllog-helper.android.js
+++ calllog-helper.android.js
@@ -8,26 +8,30 @@ exports.convertNativeCursorToJson = function(cursor) {
         //noinspection JSUnresolvedFunction
         var name = cursor.getColumnName(i);
 
-        switch (type) {
-            case 0: // NULL
-                results[name] = null;
-                break;
-            case 1: // Integer
-                //noinspection JSUnresolvedFunction
-                results[name] = cursor.getInt(i);
-                break;
-            case 2: // Float
-                //noinspection JSUnresolvedFunction
-                results[name] = cursor.getFloat(i);
-                break;
-            case 3: // String
-                results[name] = cursor.getString(i);
-                break;
-            case 4: // Blob
-                results[name] = cursor.getBlob(i);
-                break;
-            default:
-                throw new Error('CallLog - Unknown Field Type '+ type);
+        if (name === 'date') {
+            results[name] = cursor.getString(i);
+        } else {
+            switch (type) {
+                case 0: // NULL
+                    results[name] = null;
+                    break;
+                case 1: // Integer
+                    //noinspection JSUnresolvedFunction
+                    results[name] = cursor.getInt(i);
+                    break;
+                case 2: // Float
+                    //noinspection JSUnresolvedFunction
+                    results[name] = cursor.getFloat(i);
+                    break;
+                case 3: // String
+                    results[name] = cursor.getString(i);
+                    break;
+                case 4: // Blob
+                    results[name] = cursor.getBlob(i);
+                    break;
+                default:
+                    throw new Error('CallLog - Unknown Field Type '+ type);
+            }
         }
     }
 
